7234d4b83d467dbc5df9c1cae8c4938c9660f6ba
Lena <mgdgri@gmail.com>
2023-04-17 00:02:30 +0200

    simplified

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,53 +1,59 @@
 from guess_the_number_art import logo
 from random import randrange
 
-print(logo)
-number = randrange(1, 101)
-print(f"The number is {number}")
+def check_answer(guess, number, attempts):
+    if guess > number:
+        attempts += -1
+        print("Too high.")
+    elif guess < number:
+        attempts += -1
+        print("Too low.")
+    elif guess == number:
+        print(f"You got it! The answer was {number}.")
+        exit()
+    return attempts
 
-def easy():
-    easy_attempts = 10
+def guess():
+    guess = input("Make a guess:")
+    if guess is int:
+        pass
+    else:
+        exit()
+
+def easy(number, easy_attempts):
     while easy_attempts > 0:
         print(f"You have {easy_attempts} attempts remaining to guess the number.")
         guess = int(input("Make a guess:"))
-        if guess > number:
-            easy_attempts += -1
-            print("Too high.")
-        elif guess < number:
-            easy_attempts += -1
-            print("Too low.")
-        elif guess == number:
-            print(f"You got it! The answer was {number}")
-            exit()
+        # guess()
+        # still missing: check if int was added and catch string input
+        easy_attempts = check_answer(guess, number, easy_attempts)
+    print("No attempts left. You lose!")
     return False
 
-def hard():
-    hard_attempts = 5
+def hard(number, hard_attempts):
     while hard_attempts > 0:
         print(f"You have {hard_attempts} attempts remaining to guess the number.")
         guess = int(input("Make a guess:"))
-        if guess > number:
-            hard_attempts += -1
-            print("Too high.")
-        elif guess < number:
-            hard_attempts += -1
-            print("Too low.")
-        elif guess == number:
-            print(f"You got it! The answer was {number}")
-            exit()
+        hard_attempts = check_answer(guess, number, hard_attempts)
+    print("No attempts left. You lose!")
     return False
 
+print(logo)
+number = randrange(1, 101)
+# print(f"The number is {number}")
 print("Welcome to the number guessing game!")
 print("I'm thinking of a number between 1 and 100.")
-  
+easy_attempts = 10
+hard_attempts = 5
+
 game_is_on = True
 while game_is_on:
     chosen_difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
     print("")
     if chosen_difficulty == "easy":
-        easy()  
+        easy(number, easy_attempts)  
     elif chosen_difficulty == "hard":
-        hard()
+        hard(number, hard_attempts)
     else:
         print("Wrong answer, please try again.")
         
