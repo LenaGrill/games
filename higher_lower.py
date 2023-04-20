1c2936a7f1f8cbe41ac62a83ff1d9fe21024f183
Lena <mgdgri@gmail.com>
2023-04-20 20:47:48 +0200

    minor corrections

--- higher_lower.py
+++ higher_lower.py
@@ -1,8 +1,9 @@
 from random import randrange
 from higher_lower_art import logo, vs
 from higher_lower_game_data import data
+from os import system
 
-print(logo)
+# to find out length of the data set, len function was used
 # print(len(data))
 
 def choose_number():
@@ -10,36 +11,39 @@ def choose_number():
   chosen_number = randrange(0, 50)
   return chosen_number
 
-def choose_person(chosen_number):
-  """ function to choose random person from game_data using chosen_number """
-  chosen_person = data[chosen_number]
-  return chosen_person
+def choose_dataset(chosen_number):
+  """ function to choose random dataset from game_data using chosen_number """
+  chosen_dataset = data[chosen_number]
+  return chosen_dataset
 
 def first_entry():
-  """ function to create first entry for the game"""
+  """ function to select first dataset for the game"""
   choiceA = (choose_number())
-  A = choose_person(choiceA)
+  A = choose_dataset(choiceA)
   a = A['follower_count']
   return A, a
 
-
 def game(A, a, score):
+  """ this is the function for the actual game """
 
+  # select second dataset for comparison
   choiceB = (choose_number())
-  B = choose_person(choiceB)
+  B = choose_dataset(choiceB)
   b = B['follower_count']
 
-  # print person A data
+  # print dataset A
   print(f"Compare A: {A['name']}, {A['description']}, {A['country']}")
 
   print(vs)
-  # print person B data
+  # print dataset B
   print(f"Against B: {B['name']}, {B['description']}, {B['country']}")
 
   # ask for input higher or lower
-  print(a, b)
   answer = input("Who has more followers? Type 'A' or 'B': ")
+  system('cls') # clears the screen
+  print(logo) # prints the logo again
 
+  # selection will be evaluated and score increased, if True: 
   if answer == 'A' and a > b:
     score += 1
     print(f"You are right! Current score: {score}")
@@ -56,9 +60,13 @@ def game(A, a, score):
     print(f"You are right! Current score: {score}")
     A, a = B, b
     return True, A, a, score
+  else:
+    print("Wrong input or same dataset was randomly selected twice. Please try again.")
+    A, a = B, b
+    return True, A, a, score
 
-
-# if right, choose next random person and repeat above
+# the actual start of the program
+print(logo)
 A, a = first_entry()
 score = 0
 play = True
